a8f898bb4f85152e423dcfdcc663102930cad007
Julio-Arroyo <jarroyoi@caltech.edu>
2024-01-19 09:00:47 -0800

    Add number sequence transmission.

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,8 +2,29 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 
-int main() {
+void transferFile(const std::string& fname) {
+  std::ifstream fileToTransfer{fname};
+  if (!fileToTransfer.is_open()) {
+    std::cerr << "Could not open file " << fname << std::endl;
+    return;
+  }
+
+  std::stringstream buf;
+  buf << fileToTransfer.rdbuf();
+  fileToTransfer.close();
+  std::string contents = buf.str();
+  std::cout << contents << std::endl;
+
+  JC::TcpSocket sock;
+  sock.open(JC::SocketType::TCP_INITIATOR, SERVER_PORT, SERVER_IP_ADDR);
+  sock.write(static_cast<void*>(contents.data()), contents.length());
+  sock.teardown();
+}
+
+void transmitNumberSequence(const int sequence_len) {
   JC::TcpSocket sock;
 
   // connect to server
@@ -12,12 +33,31 @@ int main() {
             SERVER_IP_ADDR);
 
   // block so that server has time to startu up
-  std::cout << "Type a message to send to the Server: " << std::endl;
-  std::string msg;
-  std::cin >> msg;
-  sock.write(static_cast<void*>(msg.data()), msg.length());
+  // std::cout << "Type a message to send to the Server: " << std::endl;
+  // std::string msg;
+  // std::cin >> msg;
+
+  char buf[sequence_len];
+  char epoch = 0;
+  std::cout << (int) epoch << std::endl;
+  int count = 0;
+  while (count < sequence_len) {
+    buf[count++] = epoch;
+    if (count % 256 == 0) {
+      epoch++;
+    }
+  }
+  std::cout << (int) epoch << std::endl;
+
+  sock.write(static_cast<void*>(buf), sequence_len);
 
   sock.teardown();
+}
+
+int main() {
+  // transferFile("../inc/JcTcpSocket.hpp");
+
+  transmitNumberSequence(1800);
 
   return 0;
 }
--- src/Server.cpp
+++ src/Server.cpp
@@ -2,25 +2,55 @@
 
 #include <iostream>
 
-#define BUF_SIZE 256
+#define BUF_SIZE 2000 
+#define MAX_ATTEMPTS 100
 
-int main() {
+void receiveFile() {
   JC::TcpSocket sock;
   sock.open(JC::SocketType::TCP_LISTENER, SERVER_PORT, SERVER_IP_ADDR);
 
-  JC::TcpHeader dummyHeader;
-  std::cout << "sizeof(JC::TcpHeader):" << sizeof(dummyHeader) << std::endl;
+  char line[BUF_SIZE];
+  int file_size = 0;
+  while (true) {
+    file_size += sock.read(static_cast<void*>(line + file_size), BUF_SIZE, JC::ReadMode::BLOCK);
+    std::string line_str(line + file_size);
+    std::cout << line_str << std::endl;
+
+    std::cout << "line_len " << file_size << std::endl;
+  }
+  // size_t attempts = 0;
+  // while (line_len > 0 || attempts++ < MAX_ATTEMPTS) {
+  //   std::cout << line << std::endl;
+  //   line_len = sock.read(static_cast<void*>(line), BUF_SIZE, JC::ReadMode::NO_WAIT);
+  // }
+  sock.teardown();
+}
+
+void receiveNumberSequence() {
+  JC::TcpSocket sock;
+  sock.open(JC::SocketType::TCP_LISTENER, SERVER_PORT, SERVER_IP_ADDR);
+
+  std::string dummyStr;
+  std::cout << "type something to continue" << std::endl;
+  std::cin >> dummyStr;
 
   char msg[BUF_SIZE];
   int msg_size = sock.read(static_cast<void*>(msg), BUF_SIZE, JC::ReadMode::BLOCK);
-
+  std::cout << "msg size" << msg_size << std::endl;
   std::cout << "Client message: " << std::endl;
   for (int i = 0; i < msg_size; i++) {
-    std::cout << msg[i];
+    int num = msg[i];
+
+    std::cout << (255*num + num) << ","; 
   }
   std::cout << std::endl;
 
   sock.teardown();
+}
 
+int main() {
+  // receiveFile();
+
+  receiveNumberSequence();
   return 0;
 }
